fe8eaee09428ea66431d3f3fadcc2f25aa2070ae
Conner Crosby <crosbyco3@gmail.com>
2019-05-24 13:06:37 -0400

    forgot to add main to last commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,15 @@
 #include <iomanip>
 #include <string>
 #include <sstream>
+#include <vector>
 #include <windows.h>
 #include "Main_Menu.h"
 
 int main() {
+    std::vector<Student_Record> student_records{};
     const int total_buffer_width {120}; // 120 is the default for cmd, might look into other default terminal widths
     bool done {false};
-    char exit_char {};
+    std::string exit_key {};
     print_menu(total_buffer_width);
 
     do{
@@ -16,7 +18,7 @@ int main() {
         if(!is_valid_input(input, total_buffer_width))
             continue;
 
-        done = select_option(input, total_buffer_width);
+        done = select_option(input, total_buffer_width, student_records);
 
     } while (!done);
 
@@ -25,7 +27,7 @@ int main() {
     std::cout << "Comments can be emailed to: <recipient>@<email>.com" << std::endl;
     std::cout << std::endl;
     std::cout << "Enter any key to exit..." << std::endl;
-    std::cin >> exit_char;
+    std::getline(std::cin, exit_key);
 
     return 0;
 }
